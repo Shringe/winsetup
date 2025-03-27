87917ae0d7d221280cbb4c6036e15a7447538bfc
Logen Deamicis <logen.deamicis@valleyviewisd.net>
2025-03-27 09:06:33 -0500

    infastructure

--- src/platforms/windows.rs
+++ src/platforms/windows.rs
@@ -1,2 +1,20 @@
+use std::Path;
+use std::File;
+use std::env;
+
+
+struct Scoop {
+    script: Path,
+}
+
+impl Scoop {
+    pub fn download(&self) {
+
+    }
+}
+
 pub fn install() {
+    let scoop = Scoop {
+        script: Path::new("),
+    }
 }
