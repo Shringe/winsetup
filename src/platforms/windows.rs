bb52dfae25024fade40f0d6db5e2efd061d3958d
Logen Deamicis <logen.deamicis@valleyviewisd.net>
2025-03-28 09:07:14 -0500

    reqwest start

--- src/platforms/windows.rs
+++ src/platforms/windows.rs
@@ -1,9 +1,11 @@
 use std::path::PathBuf;
 use std::env;
-
+use std::io::Cursor;
+use reqwest::{blocking::Client, blocking::Response, Url};
 
 struct DownloadManager {
     dir: PathBuf,
+    client: Client,
 }
 
 impl DownloadManager {
@@ -11,12 +13,25 @@ impl DownloadManager {
         let mut dir = env::temp_dir();
         dir.push("winsetup");
 
+        let http_client_builder = Client::builder();
+        let http_client = http_client_builder.build().unwrap();
+
         Self {
             dir: dir,
+            client: http_client,
         }
     }
 
-    pub fn get_file(&self, url: &str) {
+    pub fn get_file(&self, url: String, file_name: String) {
+        let response = self.client.get(url).send();
+
+        if response.is_err() {
+                panic!("Error while retrieving data: {:#?}", response.err());
+        } else {            
+               let body = response.unwrap().bytes().unwrap();
+               // Get the content of the response
+               std::fs::write(file_name , &body).expect("Reference proteome download failed for {file_name}");
+        }
     }
 }
 
@@ -33,6 +48,7 @@ impl Scoop {
     }
 
     pub fn download(&self) {
+        self.manager.get_file("https://github.com/jtroo/kanata/releases/download/v1.8.0/kanata.exe".to_string(), "kanata.exe.test".to_string());
     }
 }
 
