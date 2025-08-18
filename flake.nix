794f0e91e27ca87fbb9efd871bc23f6748de3311
Shringe <dashingkoso@gmail.com>
2025-08-18 11:24:34 -0500

    added flake

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-25.05";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+        name = cargoToml.package.name;
+        version = cargoToml.package.version;
+      in {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          inherit name version;
+
+          cargoLock.lockFile = ./Cargo.lock;
+          src = self;
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            git
+            rustc
+            rustfmt
+            cargo
+            rust-analyzer
+          ];
+        };
+      }
+    );
+}
