113d5b47910406fc20771da11d0795aac7c19693
Shringe <dashingkoso@gmail.com>
2025-08-19 10:09:57 -0500

    basic cross compilation

--- flake.nix
+++ flake.nix
@@ -1,25 +1,64 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/release-25.05";
-    flake-utils.url  = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    fenix.url = "github:nix-community/fenix";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      fenix,
+      naersk,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs {
           inherit system;
         };
 
+        toolchain =
+          with fenix.packages.${system};
+          combine [
+            minimal.rustc
+            minimal.cargo
+            targets.x86_64-pc-windows-gnu.latest.rust-std
+          ];
+
+        naersk' = naersk.lib.${system}.override {
+          cargo = toolchain;
+          rustc = toolchain;
+        };
+
         cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
         name = cargoToml.package.name;
         version = cargoToml.package.version;
-      in {
-        packages.default = pkgs.rustPlatform.buildRustPackage {
+      in
+      rec {
+        packages.default = packages.windows;
+
+        packages.linux = pkgs.rustPlatform.buildRustPackage {
           inherit name version;
 
           cargoLock.lockFile = ./Cargo.lock;
           src = self;
+        };
+
+        packages.windows = naersk'.buildPackage {
+          src = self;
+          strictDeps = true;
+          doCheck = false;
+
+          depsBuildBuild = with pkgs; [
+            pkgsCross.mingwW64.stdenv.cc
+            pkgsCross.mingwW64.windows.pthreads
+          ];
+
+          CARGO_BUILD_TARGET = "x86_64-pc-windows-gnu";
         };
 
         devShells.default = pkgs.mkShell {
